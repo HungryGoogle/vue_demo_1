0d310c0f76ad22e2f2f990b4b782236608cd63af
liwenqing <liwenqing2008@126.com>
2022-04-25 19:17:51 +0800

    liwenqing 20220424 springboot getIp ok.

--- src/main/java/com/example/test/util/IpUtil.java
+++ src/main/java/com/example/test/util/IpUtil.java
@@ -1,9 +1,13 @@
 package com.example.test.util;
 
+import eu.bitwalker.useragentutils.UserAgent;
+
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
- 
+
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+
 /**
  * @Author : JCccc
  * @CreateTime : 2018-11-23
@@ -42,10 +46,46 @@ public class IpUtil {
                 }
             }
         } catch (Exception e) {
-            ipAddress="";
+            ipAddress = "";
         }
         // ipAddress = this.getRequest().getRemoteAddr();
- 
+
         return ipAddress;
+    }
+
+
+    //    UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("user-agent"));
+//    String clientType = userAgent.getOperatingSystem().getDeviceType().toString();
+//        LOGGER.info("clientType = " + clientType);   //客户端类型  手机、电脑、平板
+//    String os = userAgent.getOperatingSystem().getName();
+//        LOGGER.info("os = " + os);    //操作系统类型
+//    String ip = IpUtil.getIpAddr(request);
+//        LOGGER.info("ip = " + ip);    //请求ip
+//    String browser = userAgent.getBrowser().toString();
+//        LOGGER.info("browser = " + browser);    浏览器类型　　//其中ip的获取方式
+    public static String getIpAddr2(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");            //LOGGER.error("X-Real-IP:"+ip);
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("http_client_ip");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }        // 如果是多级代理，那么取第一个ip为客户ip
+        if (ip != null && ip.indexOf(",") != -1) {
+            ip = ip.substring(ip.lastIndexOf(",") + 1, ip.length()).trim();
+        }
+        return ip;
     }
 }
