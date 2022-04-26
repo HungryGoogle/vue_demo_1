b110f486032b3f035d422347cd262134b372217a
Administrator <liwenqing2008@126.com>
2022-04-26 10:31:37 +0800

    liwenqing 1. download file succeed 20220426

--- src/main/java/com/example/test/bean/DishBean.java
+++ src/main/java/com/example/test/bean/DishBean.java
@@ -3,7 +3,7 @@ package com.example.test.bean;
 public class DishBean {
     private int id;
     private String whichWeekDay;    //周几
-    private String mealtime;        //时间段，早餐、中餐、晚餐、夜宵
+    private String mealtime;        //餐名，早餐、中餐、晚餐、夜宵
     private String dishName;        //菜名
     private String dishType;        //热菜、凉菜、汤、炖菜
     private float dishPrice;       // 菜的价格
--- src/main/java/com/example/test/controller/FileController.java
+++ src/main/java/com/example/test/controller/FileController.java
@@ -4,12 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -27,7 +31,7 @@ public class FileController {
 
 
     @Value("${file.upload.dir}")
-    private String realPath;
+    private String upLoadDirPath;
 
     /**
      * 第二种文件上传
@@ -52,42 +56,32 @@ public class FileController {
         String newFileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS-").format(new Date()) + UUID.randomUUID() + ext;
 
         // 上传文件到哪
-        file.transferTo(new File(realPath, newFileName));
+        file.transferTo(new File(upLoadDirPath, newFileName));
         return "redirect:/";
     }
 
+
+    @Value("${file.download.dir}")
+    private String downloadRealPath;
 
     /**
-     * 用来测试文件上传 - 这种方式：不能用于jar包部署
-     * 注意：这种方式存在局限性, 不推荐再使用这种方式进行文件上传了
-     *
-     * @return
+     * 文件下载
+     * @param fileName
      */
-    /*
-    @RequestMapping("/uploadFile")
-    // 定义：接收文件对象 MultipartFile file变量名要与form表单中input type="file" 标签name属性名一致
-    public String uploadFileTest(MultipartFile file, HttpServletRequest request) throws IOException {
+    @RequestMapping("/download")
+    public void downloadFile(String fileName, HttpServletResponse response) throws Exception {
+        log.debug("当前下载的文件名是：{}", fileName);
+        log.debug("当前下载的文件的目录是：{}", downloadRealPath);
+        // 1.去指定目录读取文件
+        File file = new File(downloadRealPath, fileName);
+        // 2.将文件读取为文件输入流
+        FileInputStream is = new FileInputStream(file);
+        // 2.1 获取响应流之前  一定要设置以附件形式下载
+        response.setHeader("content-disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+        // 3.获取响应输出流
+        ServletOutputStream os = response.getOutputStream();
 
-        // 文件名
-        String originalFilename = file.getOriginalFilename();
-        log.debug("文件名: {}", file.getOriginalFilename());
-        log.debug("文件大小: {}", file.getSize());
-        log.debug("文件类型: {}", file.getContentType());
-
-        // 处理文件上传
-        // 1.根据相对路径 "uploads" 获取绝对路径
-        String realPath = request.getSession().getServletContext().getRealPath("/uploads");
-        log.debug("获取的绝对路径: {}", realPath);
-
-        // 2.上传文件  参数1：将文件写入到那个目录
-        // 修改文件名
-
-        String ext = originalFilename.substring(originalFilename.lastIndexOf("."));
-        String newFileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS-").format(new Date()) + UUID.randomUUID() + ext;
-        file.transferTo(new File(realPath, newFileName));
-
-        return "redirect:/";
+        FileCopyUtils.copy(is,os);
     }
-    */
 
 }
