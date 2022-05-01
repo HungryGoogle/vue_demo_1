a7eb3c085979ce4144c5c211b08aed15bdf00797
liwenqing <liwenqing2008@126.com>
2022-05-01 10:34:44 +0800

    liwenqing 20220424 1、css中的icon图片正常引用；

--- src/main/java/com/example/test/VueDemo1Application.java
+++ src/main/java/com/example/test/VueDemo1Application.java
@@ -4,6 +4,7 @@ import com.example.test.util.config.DishConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
